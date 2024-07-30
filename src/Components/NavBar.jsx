46f5bb7691cb97e961a3fbbb8c644172a571927f
abdulsalam11-alao <abdulsalamalao89@gmail.com>
2024-07-30 15:43:18 +0100

    completed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { useTheme } from "../context/ThemeProvider";
 import "./NavBar.css";
+
 function NavBar() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <nav>
-      <h1>Where in the World ?</h1>
-      <h2>
-        <FontAwesomeIcon icon={faMoon} />
-        <span>Dark mode</span>
+      <h1>Where in the World?</h1>
+      <h2 onClick={toggleTheme} style={{ cursor: "pointer" }}>
+        <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
+        <span>{theme === "light" ? "Dark mode" : "Light mode"}</span>
       </h2>
     </nav>
   );
